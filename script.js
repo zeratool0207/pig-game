7a85833e861a14bbf2fbd7903f1d0154acd9313a
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-07 06:24:42 +0900

    pig game proceeding....

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use script';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -17,7 +20,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let scores = [0,0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click',function() {
@@ -32,10 +37,14 @@ btnRoll.addEventListener('click',function() {
     if (dice !== 1) {
         // add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // switch to next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
 
