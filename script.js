3f589c46dd296e3963e7ed70c6b4ae45cde97274
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-07 05:44:50 +0900

    pig game proceeding...

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 'use script';
 
+// Selecting elements
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('#score--1');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// String Condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
 
